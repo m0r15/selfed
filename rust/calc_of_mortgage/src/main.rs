044dd74293bed21174a407c356fc7de96a5ce07c
m0r15 <serenkovav@gmail.com>
2018-05-08 16:56:18 +0300

    add calc function

--- rust/calc_of_mortgage/src/main.rs
+++ rust/calc_of_mortgage/src/main.rs
@@ -17,10 +17,41 @@ extern crate argparse;
 
 use argparse::{ArgumentParser, StoreTrue, Store};
 
-fn pay_per_month(u64: s, ) -> u64 {
-    // TODO:
+#[derive(Debug)]
+struct Mortgage {
+    sum: f64,
+    percent: f64,
+    period: i32,
+}
+
+impl Mortgage {
+
+    fn new() -> Mortgage {
+        Mortgage {
+            sum: 0.0,
+            percent: 0.0,
+            period: 0,
+        }
+    }
+
+    fn build(gage_sum: f64, gage_percent: f64, gage_period: i32) -> Mortgage {
+        Mortgage {
+            sum: gage_sum,
+            percent: gage_percent,
+            period: gage_period,
+        }
+    }
+
+    fn pay_per_month(&self) -> f64 {
+        let p: f64 = self.percent / 12.0;
+
+        self.sum * p / (1.0 - (1.0 + p).powi(1 - self.period)) // result
+    }
+
 }
 
 fn main() {
-    
+    let mortgage = Mortgage::build(12.53, 4_200_000.0, 160);
+
+    println!("{:.2}", mortgage.pay_per_month());
 }
