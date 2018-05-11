9736089127a5ce0ff57f1c57850f6661e1d2961f
m0r15 <serenkovav@gmail.com>
2018-05-11 15:16:59 +0300

    fixed solution

--- rust/calc_of_mortgage/src/main.rs
+++ rust/calc_of_mortgage/src/main.rs
@@ -1,16 +1,5 @@
-/*
- * Калькулятор для ипотеки
- * 
- * Вычислить месячные выплаты фиксированного срока в течении 
- * заданных N сроков с заданной процентной ставкой.
- * 
- * Инуитентная схема расчета
- * 
- * x = S * p / (1 - (1 + P) ^ (1-M))
- * x - ежемесячные выплаты
- * S - сумма ипотеки
- * p - (1/12) годового %
- * M - общий период кредитования в месяцахы
+/**
+ * by m0r15 (serenkvoav@gmail.com)
  */
 
 extern crate argparse;
@@ -19,9 +8,9 @@ use argparse::{ArgumentParser, StoreTrue, Store};
 
 #[derive(Debug)]
 struct Mortgage {
-    sum: f64,
-    percent: f64,
-    period: i32,
+    sum:     f64, /// сумма ипотеки
+    percent: f64, /// процент по ипотеке (годовой)
+    period:  i32, /// период в месяцах
 }
 
 impl Mortgage {
@@ -36,22 +25,24 @@ impl Mortgage {
 
     fn build(gage_sum: f64, gage_percent: f64, gage_period: i32) -> Mortgage {
         Mortgage {
-            sum: gage_sum,
+            sum:     gage_sum,
             percent: gage_percent,
-            period: gage_period,
+            period:  gage_period,
         }
     }
 
+    /// Вычисление платежа по ипотеки в месяц
     fn pay_per_month(&self) -> f64 {
-        let p: f64 = self.percent / 12.0;
+        let i: f64 = (self.percent / 100.0) / 12.0;
 
-        self.sum * p / (1.0 - (1.0 + p).powi(1 - self.period)) // result
+        // return result
+        self.sum * ( i * (1.0 + i).powi(self.period) / ((1.0 + i).powi(self.period) - 1.0) )
     }
 
 }
 
 fn main() {
-    let mortgage = Mortgage::build(12.53, 4_200_000.0, 160);
+    let mortgage = Mortgage::build(4_200_000.0, 12.53, 160);
 
-    println!("{:.2}", mortgage.pay_per_month());
+    println!("mortgage per month: {:.2}", mortgage.pay_per_month());
 }
